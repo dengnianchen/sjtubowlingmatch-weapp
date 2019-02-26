c7aa9e978cfe268a42a84767ad958fc1f1780f04
dengnianchen <dengnianchen@sjtu.edu.cn>
2019-02-26 20:50:58 +0800

    commit
    Signed-off-by: dengnianchen <dengnianchen@sjtu.edu.cn>

--- src/models/Play.js
+++ src/models/Play.js
@@ -3,7 +3,16 @@ class Play extends $.Model {
 	constructor(data = null) {
 		super(data);
 	}
-
+	
+	static async judgeByMe() {
+		return await $.Http.request('/play/judge-by-me');
+	}
+	
+	static async judgeConfirm(e, play) {
+		if (play instanceof Object)
+			play = play.id;
+		await $.Http.submit(e, `PUT /play/${play}/judge-confirm`);
+	}
 }
 
 module.exports = Play;
