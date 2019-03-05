e5180510c4e3eb29f127b2c887d457062003b8c6
dengnianchen <dengnianchen@sjtu.edu.cn>
2019-03-05 09:32:25 +0800

    commit
    Signed-off-by: dengnianchen <dengnianchen@sjtu.edu.cn>

--- src/models/Play.js
+++ src/models/Play.js
@@ -1,3 +1,18 @@
+/**
+ * @property        {number}              id
+ * @property        {string}              match          赛事名称
+ * @property        {string}              type           比赛类型
+ * @property        {string}              status         比赛状态（new:未登记, filled:已登记, player_accepted：选手已接受，accepted: 已接受，player_refused：被选手拒绝，refused：被裁判拒绝）
+ * @property        {string}              play_date      比赛日期
+ * @property        {number}              judge_id       裁判ID
+ * @property        {string}              create_time    创建时间
+ * @property        {string}              update_time    更新时间
+ * @property-read   {User}                judge          [关联]裁判
+ * @property-read   {PlayerParticipate[]} participants   [关联]参与方
+ * @property-read   {Game[]}              games          [关联]比赛球局
+ * @property-read   {Match}               match_info     [关联]赛事信息
+ 
+ */
 class Play extends $.Model {
 	
 	constructor(data = null) {
@@ -21,6 +36,10 @@ class Play extends $.Model {
 		await $.Http.submit(e, `POST /play/${play}/judge-confirm/${accept}`);
 	}
 
+	static async getPlay(id) {
+		return await $.Http.request(`/play/${id}`, {}, {}, Play);
+	}
+	
 }
 
 module.exports = Play;
