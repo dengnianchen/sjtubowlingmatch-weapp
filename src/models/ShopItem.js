bb22f01173c5e833ada44ebbeb3a9330a42bfbf3
dengnianchen <dengnianchen@sjtu.edu.cn>
2019-03-30 19:52:27 +0800

    commit
    Signed-off-by: dengnianchen <dengnianchen@sjtu.edu.cn>

---
+++ src/models/ShopItem.js
@@ -0,0 +1,30 @@
+/**
+ * @property        {Number}    id
+ * @property        {String}    item_name       名称
+ * @property        {String}    description     描述
+ * @property        {String}    identifier      标识符
+ * @property        {String}    match           所属赛事
+ * @property        {Array}     detail          详情（内含字段与具体赛事有关）
+ * @property-read   {String}    create_time     创建时间（框架自动生成）
+ * @property-read   {String}    update_time     更新时间（框架自动生成）
+ * @property-read   {Array}     violates        违背哪几条限制条件（若没有违背则为空数组）
+ * @property-read   {Boolean}   affordable      是否有足够资源兑换
+ */
+class ShopItem extends $.Model {
+	
+	/**
+	 *
+	 * @param match
+	 * @return {Promise<ShopItem[]>}
+	 */
+	static async listAll(match) {
+		return await $.Http.request(`/${$.Model.id(match)}/shop`, {}, ShopItem);
+	}
+	
+	async buy() {
+		return await $.Http.request(`/${this.match}/shop/buy/${this.identifier}`);
+	}
+	
+}
+
+module.exports = ShopItem;
